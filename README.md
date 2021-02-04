b90d6cead372d80cc5175d932c1c935498fcbb1b
palashghosh13 <palashghosh13@gmail.com>
2021-02-04 12:23:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-material-v9-mat-select-with-mat-chip-list-kcawp2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-material-v9-mat-select-with-mat-chip-list-kcawp2)
---
+++ src/app/select-multiple-example.ts
@@ -0,0 +1,32 @@
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+/** @title Select with multiple selection */
+@Component({
+  selector: 'select-multiple-example',
+  templateUrl: 'select-multiple-example.html',
+  styleUrls: ['select-multiple-example.css'],
+})
+export class SelectMultipleExample {
+  toppingsControl = new FormControl([]);
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
+  onToppingRemoved(topping: string) {
+    const toppings = this.toppingsControl.value as string[];
+    this.removeFirst(toppings, topping);
+    this.toppingsControl.setValue(toppings); // To trigger change detection
+  }
+
+  private removeFirst<T>(array: T[], toRemove: T): void {
+    const index = array.indexOf(toRemove);
+    if (index !== -1) {
+      array.splice(index, 1);
+    }
+  }
+
+}
+
+
+/**  Copyright 2019 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
